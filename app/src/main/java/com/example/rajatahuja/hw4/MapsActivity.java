c187616a2de579b8912468c47485bf82c3317715
Rajat Ahuja <Rajat_Ahuja@utexas.edu>
2018-11-04 02:32:31 -0600

    everythign

---
+++ app/src/main/java/com/example/rajatahuja/hw4/MapsActivity.java
@@ -0,0 +1,110 @@
+package com.example.rajatahuja.hw4;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 99);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        //mMap.setBuildingsEnabled(true);
+
+
+    }
+
+    public void sendLocation(View view) {
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String name = editText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(this);
+
+        List<Address> locations;
+        try {
+            locations = geocoder.getFromLocationName(name, 1);
+
+
+            if (locations.size() == 0) {
+                editText.setText(getString(R.string.error), TextView.BufferType.EDITABLE);
+
+            } else {
+                mMap.clear();
+                Address myAddress = locations.get(0);
+
+                LatLng myLoc = new LatLng(myAddress.getLatitude(), myAddress.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(myLoc).title(myAddress.getAddressLine(0)));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(myLoc));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void zoom(View view) {
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+    }
+
+    public void zoomOut(View view) {
+        mMap.animateCamera(CameraUpdateFactory.zoomOut());
+    }
+}
